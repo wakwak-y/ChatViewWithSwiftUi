308302a3ebe316fc081be6960528a880a3e4cc0d
Yu Wakui <wak121.y@gmail.com>
2024-01-03 21:19:55 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUi/ChatViewWithSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUiApp.swift
+//  ChatViewWithSwiftUi
+//
+//  Created by 和久井侑 on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
