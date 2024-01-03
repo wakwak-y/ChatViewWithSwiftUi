2bdbe7932e6f96a2a1ce45be4be2155f9d157f71
Yu Wakui <wak121.y@gmail.com>
2024-01-03 23:07:18 +0900

    feat: Create message row

--- ChatViewWithSwiftUi/Views/MessageRow.swift
+++ ChatViewWithSwiftUi/Views/MessageRow.swift
@@ -9,16 +9,48 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack {
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top) {
+            userThumb
+            messageText
+            messageState
+            Spacer()
         }
         .padding(.bottom)
     }
 }
 
+extension MessageRow {
+    private var userThumb: some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .frame(width: 48, height: 48)
+    }
+    
+    private var messageText: some View {
+        Text("Hello World!!")
+            .padding()
+            .background(.white)
+            .cornerRadius(30)
+    }
+    
+    private var messageState: some View {
+        VStack(alignment: .trailing) {
+            Spacer()
+            Text("既読")
+            Text(formattedDateString)
+        }
+        .foregroundColor(.secondary)
+        .font(.footnote)
+    }
+    
+    private var formattedDateString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
+    }
+}
+
 #Preview {
     MessageRow()
+        .background(.cyan)
 }
